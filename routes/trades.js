b7d42de487381b4ee090dec12793e3142015eb0a
develdoe <andreeray@live.com>
2024-10-26 15:57:55 +0200

    Update at 2024-10-26T13:57:55.499Z

--- routes/trades.js
+++ routes/trades.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Trade = require('../models/Trade');
 const auth = require('../middleware/auth');
-
 const TradeSummary = require("../models/TradeSummary");
 
 // @route   GET /api/trades
