f12777c153b3ee74ade8cdb563da000cb330bf46
develdoe <andreeray@live.com>
2024-10-26 13:39:38 +0200

    Update at 2024-10-26T11:39:38.109Z

---
+++ mongo_backup.bat
@@ -0,0 +1,17 @@
+@echo off
+:: Set the MongoDB path and backup directory
+set MONGO_PATH="D:\coding\24\vue\moms\mongo_db"  :: Update this path to your MongoDB installation
+set BACKUP_DIR=D:\coding\24\vue\moms\mongo_backups
+
+:: Get the current date in YYYY-MM-DD format
+for /f "tokens=1-3 delims=-/ " %%a in ("%date%") do set CUR_DATE=%%c-%%a-%%b
+
+:: Create a folder with today's date
+set BACKUP_PATH=%BACKUP_DIR%\%CUR_DATE%
+if not exist %BACKUP_PATH% mkdir %BACKUP_PATH%
+
+:: Run the MongoDB backup command
+%MONGO_PATH%\mongodump --db moms --out %BACKUP_PATH%
+
+echo Backup completed for database 'moms' on %CUR_DATE%.
+pause
