d7fb89ee585933bc461962b1410206c5002f9709
develdoe <andreeray@live.com>
2024-10-28 08:15:51 +0100

    Update at 2024-10-28T07:15:51.208Z

--- mongo_backup.bat
+++ mongo_backup.bat
@@ -3,15 +3,22 @@
 set MONGO_PATH=D:\coding\24\vue\moms\mongo_db 
 set BACKUP_DIR=D:\coding\24\vue\moms\mongo_db\mongo_backups
 
+:: Verify if mongodump exists in the specified path
+if not exist "%MONGO_PATH%\mongodump.exe" (
+    echo ERROR: mongodump.exe not found in %MONGO_PATH%
+    pause
+    exit /b
+)
+
 :: Get the current date in YYYY-MM-DD format
 for /f "tokens=1-3 delims=-/ " %%a in ("%date%") do set CUR_DATE=%%c-%%a-%%b
 
 :: Create a folder with today's date
 set BACKUP_PATH=%BACKUP_DIR%\%CUR_DATE%
-if not exist %BACKUP_PATH% mkdir %BACKUP_PATH%
+if not exist "%BACKUP_PATH%" mkdir "%BACKUP_PATH%"
 
 :: Run the MongoDB backup command
-%MONGO_PATH%\mongodump --db moms --out %BACKUP_PATH%
+"%MONGO_PATH%\mongodump" --db moms --out "%BACKUP_PATH%"
 
 echo Backup completed for database 'moms' on %CUR_DATE%.
 pause
